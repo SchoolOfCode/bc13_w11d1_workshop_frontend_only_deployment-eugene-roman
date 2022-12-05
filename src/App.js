ed7cc0d19b99fc914c8ad484a3cd36908d4b766c
itsevgenii <smazanovich@icloud.com>
2022-12-05 15:12:21 +0000

    Added some styling

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
 import "./App.css";
+import Header from "./Component/Header/Header";
 
 function App() {
   return (
     <div className="App">
+    <Header/>
       <h1>Hello world!</h1>
     </div>
   );
---
+++ src/Component/Header/Header.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import "./Header.css"
+
+const Header = () => {
+    return (
+        <div className='header-container'>
+            <h1>Ugly h1 heading</h1>
+        </div>
+    )
+}
+
+export default Header
